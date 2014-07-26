0fe60217397b2a4992de687e8e468e1576f8780a
KINOSHITA Minoru <kinoshita@fos.kuis.kyoto-u.ac.jp>
2014-07-26 19:03:49 +0900

    Fixed a bug

--- gcc_simulator/simulator.rb
+++ gcc_simulator/simulator.rb
@@ -12,6 +12,13 @@ class Simulator
     @reg = Array.new(8, 0)
     @mem = Array.new(256, 0)
     @program = open(filename).read.split("\n")
+
+    # delete no operation lines
+    @program = @program.map{|line|
+      c = line.index(';')
+      line = line.slice(0, c) if c
+      line.strip
+    }.reject {|item| item == ""}
   end
 
   def log(str); puts str if DEBUG; end
@@ -70,22 +77,24 @@ class Simulator
   def unaop (dest, func)
     val = self.extract_val(dest)
     ret = func.call(val)
+
     if dest =~ /^\[([^\]]+)\]$/
-      if $1 =~ /^(\d+)$/
+      r = $1
+      if r =~ /^(\d+)$/
         @mem[$1.to_i] = ret
-      elsif $1 == "pc"
+      elsif r == "pc"
         @mem[@pc] = ret
-      elsif $1 =~ /^([a-h])$/
-        @mem[@reg[$1 - 'a']] = ret
+      elsif r =~ /^([a-h])$/
+        @mem[@reg[$1.ord - 'a'.ord]] = ret
       end
     else
-      if $1 =~ /^(\d+)$/
+      if dest =~ /^(\d+)$/
         error "Line #{@pc}: mov dest must not be a constant; #{dest}"
         return false
-      elsif $1 == "pc"
+      elsif dest == "pc"
         error "Line #{@pc}: mov dest must not be a constant; #{dest}"
         return false
-      elsif $1 =~ /^([a-h])$/
+      elsif dest =~ /^([a-h])$/
         @reg[$1.ord - 'a'.ord] = ret
       end
     end
@@ -95,66 +104,59 @@ class Simulator
   def run
     loops = 0
 
-    @pc = 0
+    @pc = 1
     while @pc < @program.length && loops < 1024
-      @pre_pc = @pc
-      line = @program[@pc]
-      # comment out
-      c = line.index(';')
-      line = line.slice(0, c) if c
-      line = line.strip
-      if line == ""
-        @pc += 1
-        next
-      end
 
-      if line =~ /mov\s+([^\s]+)\s*,\s*([^\s]+)/i
+      pre_pc = @pc
+      line = @program[@pc]
+
+      if line =~ /^mov\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "mov #{$1},#{$2}"
         break if ! binop $1, $2, lambda {|a, b| b}
-      elsif line =~ /inc (.+)/i
-        self.log "inc"
+      elsif line =~ /^inc\s+([^\s]+)/i
+        self.log "inc #{$1}"
         break if ! unaop $1, lambda {|a| (a + 1) % MOD}
-      elsif line =~ /dec (.+)/i
-        self.log "dec"
+      elsif line =~ /^dec\s+([^\s]+)/i
+        self.log "dec #{$1}"
         break if ! unaop $1, lambda {|a| (a - 1) % MOD}
-      elsif line =~ /add\s+([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^add\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "add"
         binop $1, $2, lambda {|a, b| (a + b) % MOD}
-      elsif line =~ /sub\s+([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^sub\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "sub"
         binop $1, $2, lambda {|a, b| (a - b) % MOD}
-      elsif line =~ /mul\s+([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^mul\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "mul"
         binop $1, $2, lambda {|a, b| (a * b) % MOD}
-      elsif line =~ /div\s+([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^div\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "div"
         binop $1, $2, lambda {|a, b| (a / b) % MOD}
-      elsif line =~ /and\s+([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^and\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "and"
         binop $1, $2, lambda {|a, b| (a & b) % MOD}
-      elsif line =~ /or\s+([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^or\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "or"
         binop $1, $2, lambda {|a, b| (a | b) % MOD}
-      elsif line =~ /xor\s+([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^xor\s+([^\s]+)\s*,\s*([^\s]+)/i
         self.log "xor"
         binop $1, $2, lambda {|a, b| (a ^ b) % MOD}
-      elsif line =~ /jlt\s+([^\s]+)\s*,\s*([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^jlt\s+([^\s]+)\s*,\s*([^\s]+)\s*,\s*([^\s]+)/i
         self.log "jlt"
         targ, x, y = $1.to_i, self.extract_val($2), self.extract_val($3)
         # $1 must be constant integer
-        @pc = targ if x > y
-      elsif line =~ /jeq\s+([^\s]+)\s*,\s*([^\s]+)\s*,\s*([^\s]+)/i
+        @pc = targ if x < y
+      elsif line =~ /^jeq\s+([^\s]+)\s*,\s*([^\s]+)\s*,\s*([^\s]+)/i
         self.log "jeq"
         targ, x, y = $1.to_i, self.extract_val($2), self.extract_val($3)
         # $1 must be constant integer
         @pc = targ if x == y
-      elsif line =~ /jgt\s+([^\s]+)\s*,\s*([^\s]+)\s*,\s*([^\s]+)/i
+      elsif line =~ /^jgt\s+([^\s]+)\s*,\s*([^\s]+)\s*,\s*([^\s]+)/i
         self.log "jgt"
         targ, x, y = $1.to_i, self.extract_val($2), self.extract_val($3)
         self.log "#{$2}, #{$3}, #{x}, #{y}"
         # $1 must be constant integer
-        @pc = targ if x < y
-      elsif line =~ /int\s+([^\s]+)/i
+        @pc = targ if x > y
+      elsif line =~ /^int\s+([^\s]+)/i
         self.log "int #{$1}"
         svc = $1
         if svc == "0"
@@ -194,16 +196,20 @@ class Simulator
           end
         end
 
-      elsif line =~ /hlt/i
+      elsif line =~ /^hlt/i
         self.log "hlt"
         @cout.puts "hlt"
         break
       else
-        self.error "Line #{idx}: No such a operation; #{line}"
+        self.error "Line #{@pc}: No such a operation; #{line}"
       end
 
+      # self.log "#{@pc}(#{loops}):"
+      # self.dump
+      # self.log ""
+
       loops += 1
-      @pc += 1 if @pc == @pre_pc
+      @pc += 1 if @pc == pre_pc
     end
   end
 
@@ -211,12 +217,14 @@ class Simulator
     while true
       start = @cin.gets
       self.run
+      self.dump
     end
   end
 
   def dump
-    puts "pc = #{@pc}"
-    puts @reg.join(',')
+    self.log "pc = #{@pc}"
+    self.log @reg.join(',')
+    self.log @mem.slice(0, 4).join(',')
   end
 end
 
